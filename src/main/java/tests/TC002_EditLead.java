cb020948a046e0d307f0d41b14fe0d35bfa362eb
Vijayalaxmi08 <69340552+Vijayalaxmi08@users.noreply.github.com>
2022-03-30 22:52:39 +0530

    End To End

---
+++ src/main/java/tests/TC002_EditLead.java
@@ -0,0 +1,42 @@
+package tests;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import hooks.TestNgHooks;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class TC002_EditLead extends TestNgHooks{
+	
+	@BeforeTest
+	public void setData() {
+		testCaseName = "TC002EditLead";
+		testDescription = "Login to Leaftaps";
+		category = "smoke";
+		authors = "Viji";  
+		dataSheetName = "TC002";
+		nodes = "Leads";
+	}
+	
+
+	@Test(dataProvider="fetchData")
+	public void createLead(String companyName, String firstName, String lastName) {
+		new HomePage()
+		.clickCRMSFA()
+		.clickLeadsTab()
+		.clickCreateLeadLink()
+		.typeCompanyName(companyName)
+		.typeFirstName(firstName)
+		.typeLastName(lastName)
+		.clickCreateLead()
+		.verifyTitle();
+	}
+	
+	
+}
